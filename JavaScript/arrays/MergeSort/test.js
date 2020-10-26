1bd3e5260e2b7a4b40d9c0f27ff40c588a74db3f
Veera Pallati <pkondalu@gmail.com>
2020-10-26 17:57:34 +0530

    Fixing Test cases

--- JavaScript/arrays/MergeSort/test.js
+++ JavaScript/arrays/MergeSort/test.js
@@ -4,11 +4,16 @@ test("Merge Sorting Using Extra Memory", () => {
     let nums = [5, 2, 3, 1];
     let result = mergeSort(nums, 1);
     console.log(result);
+    expect(result).toMatchObject(nums.sort((a, b) => a - b));
+
     result = mergeSort(nums, 2);
-    console.log(result);
-    //expect(result).toMatchObject([5, 3, 2, 1]);
-    //
-    // nums = [1, 5, 5, 7, 4545, 45, 45];
-    // result = mergeSort(nums,1);
-    // expect(result).toMatchObject([4545, 45, 45, 7, 5, 5, 1]);
+    expect(result).toMatchObject(nums.sort((a, b) => b - a)); ///
+
+    nums = [1, 5, 5, 7, 4545, 45, 45];
+    result = mergeSort(nums, 1);
+    expect(result).toMatchObject(nums.sort((a, b) => a - b));
+
+    nums = [1, 5, 5, 7, 4545, 45, 45];
+    result = mergeSort(nums, 2);
+    expect(result).toMatchObject(nums.sort((a, b) => b - a));
 });
