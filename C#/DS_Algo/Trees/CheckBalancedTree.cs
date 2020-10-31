46cc35a39590b220f0281fd05c745f01d58197f4
Veera Pallati <pkondalu@gmail.com>
2020-10-31 10:38:30 +0530

    Adding IsBalancedTree Check

---
+++ C#/DS_Algo/Trees/CheckBalancedTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace ds_algo.Trees
+{
+    public class CheckBalancedTree
+    {
+        public int checkHeight(Node root)
+        {
+            if (root == null)
+                return -1;
+
+            var left = checkHeight(root.left);
+            if (left == int.MinValue) // Here we can throw exception for if it is not balanced.
+                return left;
+
+            var right = checkHeight(root.right);
+            if (right == int.MinValue)
+                return left;
+
+            if (Math.Abs(left - right) > 1)
+                return int.MinValue;
+            return Math.Max(left, right) + 1;
+        }
+        public bool isBalanced(Node root)
+        {
+            return checkHeight(root) != int.MinValue;
+        }
+    }
+}
