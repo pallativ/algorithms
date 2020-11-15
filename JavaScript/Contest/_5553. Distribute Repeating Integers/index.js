3a7c20117c40fe60fd9bfc49c5c1272c484b4ad3
Veera Pallati <pkondalu@gmail.com>
2020-11-15 13:12:39 +0530

    Adding Back Tracking problem....

--- JavaScript/Contest/_5553. Distribute Repeating Integers/index.js
+++ JavaScript/Contest/_5553. Distribute Repeating Integers/index.js
@@ -6,39 +6,29 @@ const canDistribute = function (nums, quantity) {
         else
             map.set(num, map.get(num) + 1);
     }
+    let counts = [];
+    for (let key of map.keys()) {
+        counts.push(map.get(key));
+    }
+    counts.sort((a, b) => a - b);
+    quantity.sort((a, b) => b - a);
 
-    const takeEqual = function (customerId) {
-        for (let key of map.keys()) {
-            if (map.get(key) === quantity[customerId]) {
-                map.set(key, map.get(key) - quantity[customerId]);
-                result[customerId] = true;
-                return true;
+    const solve = function (customerId) {
+        if (customerId >= quantity.length)
+            return true;
+        for (let i = 0; i < counts.length; i++) {
+            if (counts[i] >= quantity[customerId]) {
+                counts[i] -= quantity[customerId];
+                if (solve(customerId + 1)) {
+                    return true;
+                }
+                counts[i] += quantity[customerId];
             }
         }
         return false;
     }
-    const takeGreater = function (customerId) {
-        for (let key of map.keys()) {
-            if (map.get(key) >= quantity[customerId]) {
-                map.set(key, map.get(key) - quantity[customerId]);
-                result[customerId] = true;
-                return true;
-            }
-        }
-        return false;
-    }
-    let result = new Array(quantity.length).fill(false);
-    for (let customerId = 0; customerId < quantity.length; customerId++) {
-        takeEqual(customerId);
-    }
-    for (let customerId = 0; customerId < quantity.length; customerId++) {
-        if (result[customerId] === false)
-            takeGreater(customerId);
-    }
-    for (let item of result) {
-        if (item === false)
-            return false;
-    }
-    return true;
+
+    return solve(0);
+
 };
 module.exports = {canDistribute}
