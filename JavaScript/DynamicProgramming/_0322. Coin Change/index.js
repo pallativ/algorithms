d4107c38b8756acafc9b3a5b4699b57134cacf7d
Veera Pallati <pkondalu@gmail.com>
2020-11-18 21:13:51 +0530

    Adding traces to coin change

--- JavaScript/DynamicProgramming/_0322. Coin Change/index.js
+++ JavaScript/DynamicProgramming/_0322. Coin Change/index.js
@@ -10,6 +10,7 @@ const coinChange = function (coins, amount) {
 
         /// For every remaining, we need to try all denominations.
         for (let coin of coins) {
+            tree.unshift({sum: remaining, rem: remaining - coin, coin: coin});
             let res = coinChangeRecursive(remaining - coin);
             if (res >= 0 && min > res) {
                 min = 1 + res;
@@ -22,8 +23,21 @@ const coinChange = function (coins, amount) {
     if (amount < 1)
         return 0;
 
+    // let usedCoinsCount = new Array(amount).fill(0);
     let counts = new Array(amount).fill(0);
-    return coinChangeRecursive(amount);
+    let tree = [];
+
+    let result = coinChangeRecursive(amount);
+    // let output = "";
+    // for (let i = 0; i < counts.length; i++) {
+    //     if (counts[i] !== 0 && counts[i] !== -1) {
+    //         output += (`${i}->${counts[i]}\n`);
+    //     }
+    // }
+    // console.log(usedCoinsCount);
+
+    //console.log(tree);
+    return result;
 }
 
 module.exports = {coinChange}
--- JavaScript/DynamicProgramming/_0322. Coin Change/test.js
+++ JavaScript/DynamicProgramming/_0322. Coin Change/test.js
@@ -3,19 +3,25 @@ const {coinChange} = require('./index')
 test("Input: coins = [1,2,5], amount = 11", () => {
     expect(coinChange([1, 2, 5], 11)).toBe(3);
 });
+//
+// test("Input: coins = [2], amount = 3", () => {
+//     expect(coinChange([2], 3)).toBe(-1);
+// });
+//
+// test("Input: coins = [1], amount = 0", () => {
+//     expect(coinChange([1], 0)).toBe(0);
+// });
+//
+// test("Input: coins = [1], amount = 1", () => {
+//     expect(coinChange([1], 1)).toBe(1);
+// });
+//
+// test("Input: coins = [1], amount = 2", () => {
+//     expect(coinChange([1], 2)).toBe(2);
+// });
 
-test("Input: coins = [2], amount = 3", () => {
-    expect(coinChange([2], 3)).toBe(-1);
-});
+// test("Input: coins = [58,92,387,421,194,208,231], amount = 7798", () => {
+//     expect(coinChange([58,92,387,421,194,208,231], 7798)).toBe(21);
+// });
 
-test("Input: coins = [1], amount = 0", () => {
-    expect(coinChange([1], 0)).toBe(0);
-});
 
-test("Input: coins = [1], amount = 1", () => {
-    expect(coinChange([1], 1)).toBe(1);
-});
-
-test("Input: coins = [1], amount = 2", () => {
-    expect(coinChange([1], 2)).toBe(2);
-});
