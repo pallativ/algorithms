74bdc682ea8d276e396c5eb378be643859536429
Veera Pallati <pkondalu@gmail.com>
2020-11-21 17:05:41 +0530

    Adding Simple Table Implementation

--- JavaScript/SimpleTable_Or_HashTable/Implementation/test.js
+++ JavaScript/SimpleTable_Or_HashTable/Implementation/test.js
@@ -21,4 +21,22 @@ test("Test - Delete", () => {
     ST.delete("A");
     expect(ST.get("A")).toBe(null);
     expect(ST.size()).toBe(1);
-})
+});
+
+
+test("Test - Value Change", () => {
+    let ST = new SimpleTable();
+    ST.put("A", 1);
+    expect(ST.size()).toBe(1);
+    expect(ST.get("A")).toBe(1);
+    ST.put("A", 1000);
+    expect(ST.get("A")).toBe(1000);
+});
+
+test("Test - put", () => {
+    let ST = new SimpleTable();
+    ST.put("C", 3);
+    ST.put("B", 2);
+    ST.put("A", 1);
+    ST.print();
+});
