42bae945151fa143d4d69d4d220d90bd52dad00e
Veera Pallati <pkondalu@gmail.com>
2020-11-21 12:41:52 +0530

    find unique ways in case of obstacles.

--- JavaScript/DynamicProgramming/_0063. Unique Paths II/index.js
+++ JavaScript/DynamicProgramming/_0063. Unique Paths II/index.js
@@ -1,15 +1,25 @@
 const uniquePathsWithObstacles = function (obstacleGrid) {
-    let dp = Array(obstacleGrid.length).fill(obstacleGrid[0].length).map(() => Array(n).fill(1));
-    //console.log(dp);
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    if (obstacleGrid[0][0] === 1)
+        return 0;
+    for (let r = 1; r < m; r++) {
+        obstacleGrid[r][0] = obstacleGrid[r][0] === 0 && obstacleGrid[r - 1][0] === 1 ? 1 : 0;
+    }
+
+    for (let c = 1; c < m; c++) {
+        obstacleGrid[0][c] = obstacleGrid[0][c] === 0 && obstacleGrid[0][c - 1] === 1 ? 1 : 0;
+    }
+
 
     for (let row = 1; row < m; row++) {
         for (let col = 1; col < n; col++) {
             if (obstacleGrid[row][col] === 0)
-                dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+                obstacleGrid[row][col] = obstacleGrid[row - 1][col] + obstacleGrid[row][col - 1];
             else
-                dp[row][col] = 0;
+                obstacleGrid[row][col] = 0;
         }
     }
-    return dp[m - 1][n - 1];
+    return obstacleGrid[m - 1][n - 1];
 };
 module.exports = {uniquePathsWithObstacles}
