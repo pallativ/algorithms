e6df262b20bcd65a524065100a7a6f9119e6caee
Veera Pallati <Veerakondalu.Pallati@thermofisher.com>
2021-07-21 11:48:06 +0530

    Adding LCS - Longest Palindrome SubString.

---
+++ C#/DS_Algo/DP/LongestPalindromeSubstring.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Datastructures.DP
+{
+    public class LongestPalindromeSubstring
+    {
+        private int maxLength = 1;
+        private int start = 0, end = 0;
+        private Dictionary<string, bool> memo = new Dictionary<string, bool>();
+        public string LongestPalindrome(string s)
+        {
+            LongestPalindrome(s, 0, s.Length - 1);
+            return s.Substring(start, maxLength);
+        }
+        public void LongestPalindrome(string s, int i, int j)
+        {
+            if (IsPalindrome(s, i, j, memo))
+            {
+                var currentPalindromeLength = j - i + 1;
+                if (currentPalindromeLength > maxLength)
+                {
+                    start = i;
+                    end = j;
+                    maxLength = currentPalindromeLength;
+                }
+            }
+            else
+            {
+                LongestPalindrome(s, i, j - 1);
+                LongestPalindrome(s, i + 1, j);
+            }
+        }
+        private bool IsPalindrome(string s, int i, int j,
+            Dictionary<string, bool> memo)
+        {
+            var key = $"{i},{j}";
+            if (memo.ContainsKey(key))
+                return memo[key];
+            if (i == j)
+            {
+                memo.Add(key, true);
+            }
+            else if (s[i] == s[j])
+            {
+                if (i + 1 == j)
+                    memo.Add(key, true);
+                else
+                    memo.Add(key, IsPalindrome(s, i + 1, j - 1, memo));
+            }
+            else
+                memo.Add(key, false);
+            return memo[key];
+        }
+    }
+
+    public class LongestPalindromeSubstringTests
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("a", "a")]
+        [InlineData("ac", "a")]
+        public void Test(string inputString, string expected)
+        {
+            var lcs = new LongestPalindromeSubstring();
+            var actual = lcs.LongestPalindrome(inputString);
+            Assert.Equal(actual, expected);
+        }
+    }
+}
---
+++ C#/DS_Algo/DP/PatternMatching.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+
+namespace Datastructures.DP
+{
+    public class PatternMatching
+    {
+
+        public bool IsMatch(string s, string p)
+        {
+            // if s or p is empty return false.
+            if (s == string.Empty || p == string.Empty)
+                return false;
+
+            // pattern contains = "*" return return true.
+            if (p == "*")
+                return true;
+            bool[,] memo = new bool[s.Length + 1, p.Length + 1];
+            // got to next first both characters are equal or pattern contains ?
+            // if the next character in pattern contains "*"
+            // we consider the * and move to the next character.
+            // or don't consider and move to the next character.
+            return IsMatch(s, p, 0, 0, memo);
+        }
+
+        public bool IsMatch(string s, string p, int sIndex, int pIndex, bool[,] memo)
+        {
+            if (memo[sIndex, pIndex])
+                return true;
+
+            bool ans = false;
+            if (pIndex == p.Length)
+                ans = (sIndex == s.Length);
+            else
+            {
+                bool first_match = sIndex < s.Length && (s[sIndex] == p[pIndex] || p[pIndex] == '.');
+
+
+                // Consider * and Skip * case.
+                if (pIndex + 1 < p.Length && p[pIndex + 1] == '*')
+                    ans = IsMatch(s, p, sIndex, pIndex + 2, memo) || 
+                        (first_match && IsMatch(s, p, sIndex + 1, pIndex, memo));
+                else
+                    ans = first_match && IsMatch(s, p, sIndex + 1, pIndex + 1, memo);
+            }
+            memo[sIndex, pIndex] = ans;
+            return ans;
+        }
+
+
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("ab", ".*", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("mississippi", "mis*is*p*.", false)]
+        public void Test(string s, string p, bool expected)
+        {
+            var actual = IsMatch(s, p);
+            Assert.Equal(actual, expected);
+        }
+    }
+}
